0cbb5cbecc312d1fa504455cbda2f4ed592556a2
Carlos Pabon <carlosjavier52@gmail.com>
2025-01-08 01:07:20 -0600

    ejercicio de normailizacion de email

--- Cadenas/src/GeneradorEmails.java
+++ Cadenas/src/GeneradorEmails.java
@@ -9,5 +9,26 @@ public class GeneradorEmails {
         //limpiar los espacios en blanco al inicio y al final
         var nombreNormalizado = nombreCompleto.strip();
         System.out.println("nombreNormalizado = " + nombreNormalizado);
+
+        //reemplazar los espacios en blanco por punto
+        nombreNormalizado = nombreNormalizado.replace(" ", ".");
+        //convertimos a minusculas
+        nombreNormalizado = nombreNormalizado.toLowerCase();
+        System.out.println("nombreNormalizado = " + nombreNormalizado);
+
+        //datos de la empresa
+        var nombreEmpresa = "Global Mentoring";
+        System.out.println("nombreEmpresa = " + nombreEmpresa);
+        var extensionDominio = ".com.mx";
+        System.out.println("extensionDominio = " + extensionDominio);
+
+        //quitamos los espacios en blanco y covertimos a minusculas
+        var nombreEmpresaNormalizado = nombreEmpresa.strip().replace(" ", ".").toLowerCase();
+        var dominioEmailNormalizacion = '@' + nombreEmpresaNormalizado + extensionDominio;
+        System.out.println("dominioEmailNormalizacion = " + dominioEmailNormalizacion);
+
+        //email completo
+        var emailNormalizado = nombreNormalizado + dominioEmailNormalizacion;
+        System.out.println("emailNormalizado = " + emailNormalizado);
     }
 }
