db247383602aa68568775ec678ece7ec8a4312f7
Carlos Pabon <carlosjavier52@gmail.com>
2025-01-08 01:18:14 -0600

    captura de datos por consola

---
+++ ManejoConsola/src/ManejoConsola.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class ManejoConsola {
+    public static void main(String[] args) {
+        // Introducir valores por consola
+
+        var consola = new Scanner(System.in);
+        System.out.println("Escribe tu nombre: ");
+        var nombre = consola.nextLine();
+        System.out.println("nombre = " + nombre);
+    }
+}
