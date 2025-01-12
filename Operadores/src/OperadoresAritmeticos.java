3519c2042bd20c2b449b6b676cd8d3290218df69
Carlos Pabon <carlosjavier52@gmail.com>
2025-01-11 22:31:52 -0600

    modulo de operadores

---
+++ Operadores/src/OperadoresAritmeticos.java
@@ -0,0 +1,26 @@
+public class OperadoresAritmeticos {
+    public static void main(String[] args) {
+        System.out.println(" *** Operadores Aritmeticos ***");
+        double a = 5, b = 3, resultado = 0;
+
+        //suma +
+        resultado = a + b;
+        System.out.println("resultado suma= " + resultado);
+
+        //resta -
+        resultado = a - b;
+        System.out.println("resultado resta= " + resultado);
+
+        //multiplicación *
+        resultado = a * b;
+        System.out.println("resultado multiplicación= " + resultado);
+
+        //division /
+        resultado = a / b;
+        System.out.println("resultado división=" + resultado);
+
+        //modulo %
+        resultado = a % b;
+        System.out.println("resultado resto= " + resultado);
+    }
+}
